06774f13319473da240307203da116e78b9eaf9f
atomi7e <artleg279@gmail.com>
2026-01-13 16:47:23 +0500

    Compliting second task

---
+++ homework/Week_5/task_2.py
@@ -0,0 +1,34 @@
+import json
+
+
+students_data = [
+    {"name": "Alice", "age": 20, "grades": [85, 90, 92]},
+    {"name": "Bob", "age": 21, "grades": [78, 80, 70]},
+    {"name": "Charlie", "age": 22, "grades": [95, 100, 98]}
+]
+
+with open("students.json", "w") as f:
+    json.dump(students_data, f, indent=4)
+
+print("File students.json created.")
+
+with open("students.json", "r") as f:
+    data = json.load(f)
+
+updated_data = []
+
+for student in data:
+    grades = student["grades"]
+    avg_grade = sum(grades) / len(grades)
+    
+    student_info = {
+        "name": student["name"],
+        "age": student["age"],
+        "average_grade": round(avg_grade, 2)
+    }
+    updated_data.append(student_info)
+
+with open("students_avg.json", "w") as f:
+    json.dump(updated_data, f, indent=4)
+
+print("Processing completed. Check the students_avg.json file")
